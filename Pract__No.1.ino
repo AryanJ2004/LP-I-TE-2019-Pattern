31067e7a64269355fd1b6de12c59a009f3f70178
Aryan N. Jain <aryanj1084@gmail.com>
2024-07-11 10:56:42 +0530

    Add files via upload

---
+++ Pract__No.1.ino
@@ -0,0 +1,34 @@
+#include <dht11.h>
+#define DHT11PIN 4
+
+dht11 DHT11;
+
+void  setup()
+{
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+ 
+}
+
+void loop()
+{
+  Serial.println();
+
+  int chk = DHT11.read(DHT11PIN);
+
+  Serial.print("Humidity (%): ");
+  Serial.println((float)DHT11.humidity, 2);
+
+  Serial.print("Temperature  (C): ");
+  Serial.println((float)DHT11.temperature, 2);
+
+  delay(2000);
+  if(DHT11.temperature>20)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);  
+  }
+
+}
