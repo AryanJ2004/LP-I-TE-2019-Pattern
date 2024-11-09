e19011e4f056b800860d2b4937c6da8926db5ca5
Aryan N. Jain <aryanj1084@gmail.com>
2024-11-09 20:59:27 +0530

    Updated Codes

--- Practical No.4/LRU.cpp
+++ Practical No.4/LRU.cpp
@@ -1,57 +1,43 @@
 #include <iostream>
 using namespace std;
 
-#define MAX_FRAMES 10 
-
-void LRUPageReplacement(int pages[], int n, int frameSize) {
-    int frame[MAX_FRAMES]; 
-    int pageFaults = 0;  
-    
-    for (int i = 0; i < frameSize; i++) {
-        frame[i] = -1; 
-    }
+int main() {
+    int pages[] = {7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2, 1, 2, 0, 1, 7, 0, 1};
+    int frameSize = 3;
+    int n = sizeof(pages) / sizeof(pages[0]);
+    int frame[frameSize] = {-1, -1, -1};  // Initialize frame with -1 (empty)
+    int pageFaults = 0;
 
     for (int i = 0; i < n; i++) {
         bool pageFound = false;
-        int lruIndex = -1;      
-        int lruCounter = 0;     
 
+        // Check if page is already in frame (hit)
         for (int j = 0; j < frameSize; j++) {
             if (frame[j] == pages[i]) {
-                pageFound = true; 
+                pageFound = true;
+                cout << "Page hit: " << pages[i] << endl;
                 break;
             }
         }
 
+        // If page not found, handle page fault
         if (!pageFound) {
-            
-            for (int j = 0; j < frameSize; j++) {
-                if (frame[j] == -1) {
+            int lruIndex = 0;
+            // Find LRU index (least recently used)
+            for (int j = 1; j < frameSize; j++) {
+                if (frame[j] == -1 || frame[j] == pages[i]) {
                     lruIndex = j;
                     break;
                 }
-                if (j == frameSize - 1) {
-                    lruIndex = lruCounter; 
-                }
-                lruCounter++;
             }
-            frame[lruIndex] = pages[i]; 
+
+            // Replace LRU or empty slot
+            frame[lruIndex] = pages[i];
             pageFaults++;
             cout << "Page fault: " << pages[i] << endl;
-        } else {
-            cout << "Page hit: " << pages[i] << endl;
         }
     }
+
     cout << "Total Page Faults: " << pageFaults << endl;
-}
-
-int main() {
-    /* You can take input for pages and framesize from user and pass it to the function accordingly*/
-    int pages[] = {7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2, 1, 20, 1, 7, 0, 1};
-    int n = sizeof(pages) / sizeof(pages[0]);
-    int frameSize = 3;
-
-    LRUPageReplacement(pages, n, frameSize);
-
     return 0;
 }
