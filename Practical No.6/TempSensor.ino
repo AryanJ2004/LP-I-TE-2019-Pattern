f019cbfdcfce560d1f1137b8613fe566bbc40f00
Aryan N. Jain <aryanj1084@gmail.com>
2024-11-10 22:27:40 +0530

    Update TempSensor.ino

--- Practical No.6/TempSensor.ino
+++ Practical No.6/TempSensor.ino
@@ -1,48 +1,30 @@
-char degree=176; 
-const int sensor=A1;
-void setup()
-{
-  pinMode(sensor, INPUT);
-  pinMode(13, OUTPUT);
-  Serial.begin(9600);
-  pinMode(7,OUTPUT);
-  pinMode(13,OUTPUT);
-  pinMode(6,OUTPUT);
+int tempPin = A0;   
+int val;             
+float v;             
+float cel;           
+int ledPin = 7;      
+
+void setup() {
+  pinMode(ledPin, OUTPUT);  
+  Serial.begin(9600);       
 }
 
-void loop()
-{
-  int tmp = analogRead(sensor);
-  float voltage = (tmp * 5.0)/1024;
-  float milliVolt = voltage * 1000;
-  
-  float tmpCel =  (milliVolt-500)/10 ;
-  Serial.print("Celsius: ");
-  Serial.print(tmpCel);
-  Serial.println(degree);
+void loop() {
+  val = analogRead(tempPin);  
+  v = val * (5.0 / 1023.0);   
+  cel = (v - 0.5) * 100;      
+  Serial.print("TEMPERATURE: ");
+  Serial.print(cel);
+  Serial.println(" C");
+
+  if (cel > 50) {
+    digitalWrite(ledPin, HIGH);  
+    delay(1000);                  
+    digitalWrite(ledPin, LOW);   
+    delay(1000);               
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+
   delay(1000);
-  if(tmpCel<40)
-  {
-    digitalWrite(7, HIGH);
-    delay(1000);
-    digitalWrite(7,LOW);
-    delay(1000);
-  }
-  else if(tmpCel>40 && tmpCel<200)
-  {
-    digitalWrite(13, HIGH);
-    delay(1000);
-    digitalWrite(13,LOW);
-    delay(1000);
-  }
-  else if(tmpCel>200)
-  {
-    digitalWrite(6, HIGH);
-    delay(1000);
-    digitalWrite(6,LOW);
-    delay(1000);
-  }
-  else{
-    digitalWrite(13,LOW);
-  }
 }
